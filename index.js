a5caa1f9a7430f13de5914207e1466583dbc905f
lukebelliveau <lbellive@thoughtworks.com>
2017-10-12 19:06:11 -0400

    fixed index.js to match eslint docs (plugin rule was not being recognized by eslint). also changed version in package.json -- going to unpublish and republish, because i can't semver -__-

--- index.js
+++ index.js
@@ -1,22 +1,26 @@
-module.exports.rules = {
-  'no-region': function(context) {
-    return {
-      Program(node) {
-        node.comments.forEach(c => {
-          if(
-            c.type !== "Line" ||
-            !(
-              c.value.includes("#region") ||
-              c.value.includes("#endregion")
-            )
-          ) {
-            return;
+module.exports = {
+  rules: {
+    'no-region': {
+      create: function(context) {
+        return {
+          Program(node) {
+            node.comments.forEach(c => {
+              if(
+                c.type !== "Line" ||
+                !(
+                  c.value.includes("#region") ||
+                  c.value.includes("#endregion")
+                )
+              ) {
+                return;
+              }
+              context.report(
+                c,
+                "Do not use vscode fold regions"
+              )
+            })
           }
-          context.report(
-            c,
-            "Do not use vscode fold regions"
-          )
-        })
+        }
       }
     }
   }
