555eade8350ce47f8c28c70f3b21221cceaf202a
lukebelliveau <lbellive@thoughtworks.com>
2017-10-12 17:24:44 -0400

    First commit

---
+++ index.js
@@ -0,0 +1,23 @@
+module.exports.rules = function(context) {
+  return {
+    Program(node) {
+      mments.forEach(c => {
+        if(
+          c.type !== "Line" ||
+          !(
+            c.value.includes("#region") ||
+            c.value.includes("#endregion")
+          )
+        ) {
+          return;
+        }
+        context.report(
+          c,
+          "Do not use vscode fold regions"
+        )
+      })
+    }
+  }
+}
+
+module.exports.schema = [];
